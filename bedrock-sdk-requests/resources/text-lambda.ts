2fb6c46e4df2ca293452fb91243c1b3f01c0cb33
Coner Murphy <coner@conermurphy.com>
2023-11-15 08:52:27 +0000

    chore: added comments and updated output

--- bedrock-sdk-requests/resources/text-lambda.ts
+++ bedrock-sdk-requests/resources/text-lambda.ts
@@ -3,10 +3,13 @@ import {
   InvokeModelCommand,
 } from '@aws-sdk/client-bedrock-runtime';
 
+// Create a new Bedrock SDK Client
 const client = new BedrockRuntimeClient();
 
 export const handler = async () => {
+  // Configure the input for the request to our Bedrock model.
   const input = {
+    // NOTE: The body differs depending on the model you are using so use the playground on the Bedrock dashboard to figure out what you need to include.
     body: JSON.stringify({
       prompt:
         'Write me a story about a car that goes on a journey to save a village',
@@ -19,14 +22,16 @@ export const handler = async () => {
     }),
     accept: 'application/json',
     contentType: 'application/json',
+    // The model you'd like to use
     modelId: 'cohere.command-text-v14',
   };
 
-  const command = new InvokeModelCommand(input);
-
   try {
-    const response = await client.send(command);
+    // Send the request to the Bedrock model
+    const response = await client.send(new InvokeModelCommand(input));
 
+    // Process the output from the Bedrock model and return the generated data.
+    // NOTE: The structure of the response also differs depending on the model used.
     const { generations } = JSON.parse(
       new TextDecoder().decode(response.body)
     ) as {
@@ -39,7 +44,7 @@ export const handler = async () => {
 
     return {
       statusCode: 200,
-      body: generations,
+      body: generations[0].text,
     };
   } catch (e) {
     return {
